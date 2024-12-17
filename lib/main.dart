d5db66b1bcb63935e393a24c13e0b9e38fd087e6
Aliyah <aliyahawwaliyah@gmail.com>
2024-12-17 17:41:08 +0700

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:progresfp1/screens/signup_screen.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'providers/category_provider.dart';
 import 'providers/note_provider.dart';
 import 'providers/user_provider.dart';
 import 'providers/search_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(NoteTakingApp());
 }
 
